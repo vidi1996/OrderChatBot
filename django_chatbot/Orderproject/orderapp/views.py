06c0e64f7c1865fcf114d1febb471917be16f6bc
Sanath Salian <sanath.salian@daimler.com>
2020-02-06 19:15:34 +0530

    Initial Commit

---
+++ django_chatbot/Orderproject/orderapp/views.py
@@ -0,0 +1,43 @@
+from chatterbot import ChatBot
+from django.http import JsonResponse
+from django.shortcuts import render
+from chatterbot.ext.django_chatterbot import settings
+from django.views.generic import View
+import json
+
+# Create your views here.
+class ChatterBotApiView(View):
+    """
+    Provide an API endpoint to interact with ChatterBot.
+    """
+
+    chatterbot = ChatBot(**settings.CHATTERBOT)
+
+    def post(self, request, *args, **kwargs):
+        """
+        Return a response to the statement in the posted data.
+
+        * The JSON data should contain a 'text' attribute.
+        """
+        input_data = json.loads(request.body.decode('utf-8'))
+
+        if 'text' not in input_data:
+            return JsonResponse({
+                'text': [
+                    'The attribute "text" is required.'
+                ]
+            }, status=400)
+
+        response = self.chatterbot.get_response(input_data)
+
+        response_data = response.serialize()
+
+        return JsonResponse(response_data, status=200)
+
+    def get(self, request, *args, **kwargs):
+        """
+        Return data corresponding to the current conversation.
+        """
+        return JsonResponse({
+            'name': self.chatterbot.name
+        })
